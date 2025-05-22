1fb4480134dc3914373cb0a3dc679b40b15a7b09
2276415469 <2276415469@qq.com>
2025-05-22 10:53:36 +0800

    Delete db directory

--- db/src/main/java/com/ning/repository/MessageinfDao.java
+++
@@ -1,7 +0,0 @@
-package com.ning.repository;
-
-import com.ning.entity.Messageinf;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  MessageinfDao extends JpaRepository<Messageinf, Long> {
-}
