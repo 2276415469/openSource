237bcd4aac053f78bda1890ddb4d17fc7145c071
2276415469 <2276415469@qq.com>
2025-05-22 10:53:29 +0800

    Delete service directory

--- service/src/main/java/com/ning/jpa/MessageinfService.java
+++
@@ -1,7 +0,0 @@
-package com.ning.jpa;
-
-import com.ning.entity.Messageinf;
-
-public interface MessageinfService {
-    Messageinf query();
-}
--- service/src/main/java/com/ning/service/WebService.java
+++
@@ -1,7 +0,0 @@
-package com.ning.service;
-
-import com.ning.bo.WebBO;
-
-public interface WebService {
-    WebBO query(WebBO webBo);
-}
